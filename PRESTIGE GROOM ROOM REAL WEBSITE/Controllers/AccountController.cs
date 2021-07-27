84c30e46a781d46af05c6c9f4acb0ae2032ea6f4
Jeremiah Filipo <ac98586@avcol.school.nz>
2021-07-28 11:08:20 +1200

    fixing errors with register page and also register button.

--- PRESTIGE GROOM ROOM REAL WEBSITE/Controllers/AccountController.cs
+++ PRESTIGE GROOM ROOM REAL WEBSITE/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PRESTIGE_GROOM_ROOM_REAL_WEBSITE.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,43 @@ namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: /<controller>/
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager)
+        {
+           this.userManager = userManager;
+           this.signInManager = signInManager;
+        }
+
+        public UserManager<IdentityUser> UserManager { get; }
+        public SignInManager<IdentityUser> SignInManager { get; }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName =  model.Email, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
