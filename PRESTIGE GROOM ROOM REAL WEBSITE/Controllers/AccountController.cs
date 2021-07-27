8b3c8c6832f14352ed60c07cdd7e96c7758a2258
Jeremiah Filipo <ac98586@avcol.school.nz>
2021-07-27 12:29:58 +1200

    Added register viewmodel and razor page for register.

---
+++ PRESTIGE GROOM ROOM REAL WEBSITE/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
