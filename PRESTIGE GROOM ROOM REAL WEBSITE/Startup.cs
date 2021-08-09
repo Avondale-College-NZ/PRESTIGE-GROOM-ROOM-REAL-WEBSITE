5597ecd620832bc2b7a844fc8a72c028a3df4673
Jeremiah Filipo <ac98586@avcol.school.nz>
2021-08-09 14:22:32 +1200

    hiding login and register button when logged in

--- PRESTIGE GROOM ROOM REAL WEBSITE/Startup.cs
+++ PRESTIGE GROOM ROOM REAL WEBSITE/Startup.cs
@@ -17,10 +17,25 @@ namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE
         }
 
         public IConfiguration Configuration { get; }
+        public class PasswordOptions
+        {
+            public int RequiredLength { get; set; } = 6;
+            public int RequiredUniqueChars { get; set; } = 1;
+            public bool RequireNonAlphanumeric { get; set; } = true;
+            public bool RequireLowercase { get; set; } = true;
+            public bool RequireUppercase { get; set; } = true;
+            public bool RequireDigit { get; set; } = true;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) 
         {
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequiredUniqueChars = 3;
+                options.Password.RequireNonAlphanumeric = false;
+            });
             services.AddControllersWithViews();
             services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
