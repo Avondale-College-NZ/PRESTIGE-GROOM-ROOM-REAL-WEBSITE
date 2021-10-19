0ec630a9f560f0ef67bc65fb331bca4d8449c07e
Jeremiah Filipo <ac98586@avcol.school.nz>
2021-10-20 11:06:53 +1300

    Fixed Registration and Login error

--- PRESTIGE GROOM ROOM REAL WEBSITE/Migrations/20211019220351_Initial.Designer.cs
+++ PRESTIGE GROOM ROOM REAL WEBSITE/Migrations/20211019220351_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PRESTIGE_GROOM_ROOM_REAL_WEBSITE.Data;
 
 namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211019220351_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
