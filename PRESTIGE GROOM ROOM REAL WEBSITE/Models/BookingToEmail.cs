104abdb8e20901304f0f5cd6c03d46605e66ab61
Jeremiah Filipo <ac98586@avcol.school.nz>
2021-10-27 11:08:57 +1300

    Implementing email verification for booking

---
+++ PRESTIGE GROOM ROOM REAL WEBSITE/Models/BookingToEmail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Http;
+using System.Net.Mail;
+
+namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE.Models
+{
+    public class BookingToEmail
+    {
+        [Required, Display(Name = "Your name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+
+        public string HaircutStyle { get; set; }
+        public string Time { get; set; }
+
+    }
+}
---
+++ PRESTIGE GROOM ROOM REAL WEBSITE/ViewModels/BookingToEmail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace PRESTIGE_GROOM_ROOM_REAL_WEBSITE.ViewModels
+{
+    public class BookingToEmailViewModel
+    {
+        [Required, Display(Name = "Your name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+
+        public string HaircutStyle { get; set; }
+
+        public string Time { get; set; }
+
+
+    }
+}
